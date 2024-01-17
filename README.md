85ecf00194e0fc4c8ca13bc65994bb0971517ee0
Brandon Phillips <phizzl3@gmail.com>
2024-01-16 20:46:21 -0800

    V0.1.0 (#2)
    * Update README.md
    - Adds PyInstaller command
    
    * Update quick_rsync_archive.py
    - Adds string formatting to account for spaces in file paths.

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Gets source directory and target directory via drag-and-drop.
 Archives source directory and its contents into target directory.
 
 Written for MacOS, but should also work on Linux.
+
+## PyInstaller build info
+
+```bash
+pyinstaller --onefile ./quick-rsync-archive/quick_rsync_archive.py
+```
--- quick-rsync-archive/quick_rsync_archive.py
+++ quick-rsync-archive/quick_rsync_archive.py
@@ -4,20 +4,25 @@ A small script to quickly generate an archive command for Rsync.
 Gets source directory and target directory via drag-and-drop.
 Archives source directory and its contents into target directory.
 
+VERSION = 0.1.0
+
 """
 
 import subprocess
 
 from dropped_file_getter import get_dropped_file
 
-
+# Gets source folder.
 print("\n Please drop the directory you would like to archive (source).")
 print(" This folder and all of its contents will be archived.")
 source = get_dropped_file()
+source = str(source).replace(" ", "\\ ")
 
+# Gets target (parent) folder.
 print("\n Please drop the directory you would like to archive into (target).")
 print(" The above folder and its contents will be archived into this folder.")
 target = get_dropped_file()
+target = str(target).replace(" ", "\\ ")
 
 # Shell command for rsync.
 subprocess.run(f"rsync -av --progress {source} {target}/", shell=True, check=False)
